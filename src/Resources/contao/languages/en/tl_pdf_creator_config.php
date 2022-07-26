31fa07d99bd6f9391bbf025377147fb535472561
Thomas Körner <t.koerner@heimrich-hannot.de>
2022-07-26 13:49:02 +0200

    removed utils bundle dependency

--- src/Resources/contao/languages/en/tl_pdf_creator_config.php
+++ src/Resources/contao/languages/en/tl_pdf_creator_config.php
@@ -1,10 +1,15 @@
 <?php
 
 /*
- * Copyright (c) 2021 Heimrich & Hannot GmbH
+ * Copyright (c) 2022 Heimrich & Hannot GmbH
  *
  * @license LGPL-3.0-or-later
  */
+
+use HeimrichHannot\PdfCreator\AbstractPdfCreator;
+use HeimrichHannot\PdfCreator\Concrete\DompdfCreator;
+use HeimrichHannot\PdfCreator\Concrete\MpdfCreator;
+use HeimrichHannot\PdfCreator\Concrete\TcpdfCreator;
 
 $lang = &$GLOBALS['TL_LANG']['tl_pdf_creator_config'];
 
@@ -30,24 +35,24 @@ $lang['title'] = ['Name', 'Set a name for the configuration.'];
 $lang['type'] = [
     0 => 'PDF library',
     1 => 'Choose a pdf library to render the pdf files.',
-    \HeimrichHannot\PdfCreator\Concrete\DompdfCreator::getType() => 'Dompdf',
-    \HeimrichHannot\PdfCreator\Concrete\MpdfCreator::getType() => 'mPDF',
-    \HeimrichHannot\PdfCreator\Concrete\TcpdfCreator::getType() => 'TCPDF',
+    DompdfCreator::getType() => 'Dompdf',
+    MpdfCreator::getType() => 'mPDF',
+    TcpdfCreator::getType() => 'TCPDF',
 ];
 $lang['filename'] = ['File name', 'Set a file name for the generated pdf files. You can use the %title% placeholder to use the title of the content to export in the file name.'];
 $lang['orientation'] = [
     0 => 'Page orientation',
     1 => 'Choose a page orientation.',
-    \HeimrichHannot\PdfCreator\AbstractPdfCreator::ORIENTATION_LANDSCAPE => 'Landscape',
-    \HeimrichHannot\PdfCreator\AbstractPdfCreator::ORIENTATION_PORTRAIT => 'Portrait',
+    AbstractPdfCreator::ORIENTATION_LANDSCAPE => 'Landscape',
+    AbstractPdfCreator::ORIENTATION_PORTRAIT => 'Portrait',
 ];
 $lang['outputMode'] = [
     0 => 'Output mode',
     1 => 'Choose how to output the pdf.',
-    \HeimrichHannot\UtilsBundle\PdfCreator\AbstractPdfCreator::OUTPUT_MODE_DOWNLOAD => 'Download',
-    \HeimrichHannot\UtilsBundle\PdfCreator\AbstractPdfCreator::OUTPUT_MODE_FILE => 'File',
-    \HeimrichHannot\UtilsBundle\PdfCreator\AbstractPdfCreator::OUTPUT_MODE_INLINE => 'Inline',
-    \HeimrichHannot\UtilsBundle\PdfCreator\AbstractPdfCreator::OUTPUT_MODE_STRING => 'String',
+    AbstractPdfCreator::OUTPUT_MODE_DOWNLOAD => 'Download',
+    AbstractPdfCreator::OUTPUT_MODE_FILE => 'File',
+    AbstractPdfCreator::OUTPUT_MODE_INLINE => 'Inline',
+    AbstractPdfCreator::OUTPUT_MODE_STRING => 'String',
 ];
 $lang['format'] = ['Page format', 'Set a page format. This could be a standardized format like A3, A4, A5 or Legal, otherwise you can specify the format in millimeter (width x height, seperated by comma, for example 180,210).'];
 $lang['fonts'] = [
@@ -60,3 +65,4 @@ $lang['fonts'] = [
 ];
 $lang['pageMargins'] = ['Page margins', 'Set the page margins.'];
 $lang['masterTemplate'] = ['PDF template', 'Choose a pdf template (master template), which will be the base template for the generated pdf files.'];
+$lang['filePath'] = ['Storage folder', 'Choose a folder where rendered pdfs should be stored.'];
